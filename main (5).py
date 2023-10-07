bc2a89b42d3d7118ab1599cdfabdf8234c1c94e5
asper165c22ug165cap034 <144431205+asper165c22ug165cap034@users.noreply.github.com>
2023-10-07 20:49:02 +0530

    Add files via upload

--- main (5).py
+++ main (5).py
@@ -1,20 +1,40 @@
-# Leap year
+'''
+Implement a function called sort_students that takes a list of student objects as input and sorts the
+list based on their CGPA (Cumulative Grade Point Average) in descending order. Each student object
+has the following attributes: name (string), roll_number (string), and cgpa (float). Test the function
+with different input lists of students.
+'''
 
-"""
-year % 4 = 0
-year % 100 != 0/
-year % 400 == 0
 
-"""
-def isLeapYear(year):
-  if(year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-    return True
-  else:
-    return False
+class Student:
 
-year = 2020
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
 
-if isLeapYear(year):
-  print('{} is a leap year.'.format(year))
-else:
-  print('{} is not leap year.'.format(year))
+
+def sort_students(student_list):
+  # Sort the list of students in descending order of CGPA
+  sorted_students = sorted(student_list,
+                           key=lambda student: student.cgpa,
+                           reverse=True)
+  # Syntax - lambda arg:exp
+  return sorted_students
+
+
+# Example usage:
+students = [
+    Student("kaviya", "A123", 7.8),
+    Student("Jayashree", "A124", 8.9),
+    Student("madhu", "A125", 9.1),
+    Student("jeevitha", "A126", 9.9),
+]
+
+sorted_students = sort_students(students)
+
+# Print the sorted list of students
+for student in sorted_students:
+  print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name,
+                                                     student.roll_number,
+                                                     student.cgpa))
